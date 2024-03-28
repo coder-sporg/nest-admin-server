4d648da66b04f1316cfb06d9b449f94b37341283
xuyansheng <xuyansheng@chiansec.com>
2024-03-28 14:02:14 +0800

    ✨ feat: 添加环境变量，配置校验

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigEnum } from './enum/config.enum';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log('config', this.configService.get(ConfigEnum.DB_PORT));
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import * as dotenv from 'dotenv';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
+console.log('envFilePath: ', envFilePath);
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [() => dotenv.config({ path: '.env' })], // 加载.env文件 允许加载多个配置文件 [自定义配置文件]
+      envFilePath: envFilePath, // 会覆盖 load 中的同名配置文件
+      // envFilePath: ['.env', '.env.development'], // 在多个文件中发现同一个变量，则第一个变量优先
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        // 验证.env文件中的变量是否符合要求
+        // 对数据库类型做校验
+        DB_TYPE: Joi.string().valid('mysql', 'postgres', 'mongodb'),
+        DB_HOST: Joi.alternatives().try(
+          Joi.string().ip(),
+          Joi.string().domain(),
+        ),
+        DB_PORT: Joi.number().default(3306).valid(3306, 3307, 3308),
+        DB_DATABASE: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
