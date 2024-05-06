7d4bdb985537635c4333656ab9ee0f47429df7e6
xuyansheng <xuyansheng@chiansec.com>
2024-05-06 17:06:42 +0800

    🦄 refactor(main): 优化逻辑

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,28 @@
-import * as fs from 'fs';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { HttpExceptionFilter } from './filters/http-exception.filter';
 
-const dir = process.cwd();
+// const dir = process.cwd();
 
-const httpsOptions = {
-  key: fs.readFileSync(path.resolve(dir, './https/ssl.key')),
-  pem: fs.readFileSync(path.resolve(dir, './https/ssl.pem')),
-};
+// const httpsOptions = {
+//   key: fs.readFileSync(path.resolve(dir, './https/ssl.key')),
+//   pem: fs.readFileSync(path.resolve(dir, './https/ssl.pem')),
+// };
 
 async function bootstrap() {
   const httpApp = await NestFactory.create(AppModule, { cors: true });
-  const httpsApp = await NestFactory.create(AppModule, {
-    cors: true,
-    httpsOptions,
-  });
+  // const httpsApp = await NestFactory.create(AppModule, {
+  //   cors: true,
+  //   httpsOptions,
+  // });
 
   // app.setGlobalPrefix('api');
   // 全局过滤器 只能有一个
   // app.useGlobalFilters(new HttpExceptionFilter());
 
   await httpApp.listen(3000);
-  await httpsApp.listen(4000);
+  // await httpsApp.listen(4000);
 }
 bootstrap();
